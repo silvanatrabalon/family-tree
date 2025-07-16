cf5b167502d0323a0bbd34450ea812c08e17f563
silvana trabalon <silvana.trabalon@gmail.com>
2025-07-16 14:08:52 -0300

    feat: linea filter y edit details

--- src/familyTreeConfig.js
+++ src/familyTreeConfig.js
@@ -1,17 +1,68 @@
-// src/familyTreeConfig.js
-
 const familyTreeData = {
-  nodes: [
-    { id: 1, pids: [2], name: "Juan", gender: "male", born: "1950", tree: "1", tags: ["green"] },
-    { id: 2, pids: [1], name: "Maria", gender: "female", born: "1952", tree: "1", tags: ["red"] },
-    { id: 3, mid: 2, fid: 1, name: "Luis", gender: "male", born: "1975", tree: "2", tags: ["green"] },
-  ],
-  nodeBinding: {
-    field_0: "name",
-    field_1: "born",
-  },
-  template: "hugo",
-  enableEditForm: true,
+    nodes: [
+        {
+            id: 1,
+            pids: [2],
+            nombre: "Juan",
+            genero: "masculino",
+            nacimiento: "1950",
+            linea: "1",
+            tags: ["green"]
+        },
+        {
+            id: 2,
+            pids: [1],
+            nombre: "Maria",
+            genero: "femenino",
+            nacimiento: "1952",
+            linea: "2",
+            tags: ["red"]
+        },
+        {
+            id: 3,
+            mid: 2,
+            fid: 1,
+            nombre: "Luis",
+            genero: "masculino",
+            nacimiento: "1975",
+            linea: "1",
+            tags: ["green"]
+        },
+    ],
+    nodeBinding: {
+        field_0: "nombre",
+        field_1: "nacimiento",
+    },
+    template: "hugo",
+    enableEditForm: true,
+    nodeTreeMenu: true,
+    nodeMenu: {
+        details: { text: "Ver Detalles" },
+        edit: { text: "Editar persona" },
+        remove: { text: "Eliminar persona" },
+    },
+    editForm: {
+        elements: [
+            { type: "textbox", label: "Nombre", binding: "nombre" },
+            { type: "textbox", label: "Año de nacimiento", binding: "nacimiento" },
+            {
+                type: "checkbox",
+                label: "¿Está en el grupo de WhatsApp?",
+                binding: "whatsapp"
+            }
+        ]
+    },
+    nodeDetails: [
+        { label: "Nombre", binding: "nombre" },
+        { label: "Año de nacimiento", binding: "nacimiento" },
+        { label: "Género", binding: "genero" },
+    ],
+    filterBy: ['linea'],
+    tags: {
+    filter: {
+        template: 'hugo' // or your custom template
+    }
+},
 };
 
 export default familyTreeData;
