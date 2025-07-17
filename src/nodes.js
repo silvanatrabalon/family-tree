1a1671dd071604a22cb9b130cd1fa44f5c814e11
silvana trabalon <silvana.trabalon@gmail.com>
2025-07-17 12:38:22 -0300

    feat: json

--- src/nodes.js
+++ src/nodes.js
@@ -1,16 +1,4 @@
 // src/nodes.js
-const numberToName = {
-    "1": "Leonarda",
-    "2": "Jose",
-    "3": "Marino",
-    "4": "Juan Rosendo",
-    "5": "Herminio",
-    "6": "Inisida",
-    "7": "Emma",
-    "8": "Rosendo",
-    "9": "Julio"
-};
-
 const nodes = [
     {
         id: 1,
@@ -32,93 +20,92 @@ const nodes = [
         id: 3,
         mid: 2,
         fid: 1,
-        nombre: numberToName["1"],
+        nombre: "Leonarda",
         genero: "femenino",
         nacimiento: "1900",
-        Descendientes: numberToName["1"],
+        Descendientes: "Leonarda",
         tags: ["Descendientes", "1"]
     },
     {
         id: 4,
         mid: 2,
         fid: 1,
-        nombre: numberToName["2"],
+        nombre: "Jose",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["2"],
+        Descendientes: "Jose",
         tags: ["Descendientes", "2"]
     },
     {
         id: 5,
         mid: 2,
         fid: 1,
-        nombre: numberToName["3"],
+        nombre: "Marino",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["3"],
+        Descendientes: "Marino",
         tags: ["Descendientes", "3"]
     },
     {
         id: 6,
         mid: 2,
         fid: 1,
-        nombre: numberToName["4"],
+        nombre: "Juan Rosendo",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["4"],
+        Descendientes: "Juan Rosendo",
         tags: ["Descendientes", "4"]
     },
     {
         id: 7,
         mid: 2,
         fid: 1,
-        nombre: numberToName["5"],
+        nombre: "Herminio",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["5"],
+        Descendientes: "Herminio",
         tags: ["Descendientes", "5"]
     },
     {
         id: 8,
         mid: 2,
         fid: 1,
-        nombre: numberToName["6"],
+        nombre: "Inisida",
         genero: "femenino",
         nacimiento: "1900",
-        Descendientes: numberToName["6"],
+        Descendientes: "Inisida",
         tags: ["Descendientes", "6"]
     },
     {
         id: 9,
         mid: 2,
         fid: 1,
-        nombre: numberToName["7"],
+        nombre: "Emma",
         genero: "femenino",
         nacimiento: "1900",
-        Descendientes: numberToName["7"],
+        Descendientes: "Emma",
         tags: ["Descendientes", "7"]
     },
     {
         id: 10,
         mid: 2,
         fid: 1,
-        nombre: numberToName["8"],
+        nombre: "Rosendo",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["8"],
+        Descendientes: "Rosendo",
         tags: ["Descendientes", "8"]
     },
     {
         id: 11,
         mid: 2,
         fid: 1,
-        nombre: numberToName["9"],
+        nombre: "Julio",
         genero: "masculino",
         nacimiento: "1900",
-        Descendientes: numberToName["9"],
+        Descendientes: "Julio",
         tags: ["Descendientes", "9"]
     }
-
 ];
 
 export default nodes;
